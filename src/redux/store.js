6b1cf09da4e8a811f2845d32992768322e09a4e9
Oleksii <47codemonkey@gmail.com>
2024-02-01 14:23:10 +0200

    added devtoolsenhancer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,6 @@
 import { createStore } from 'redux';
+import { devToolsEnhancer } from '@redux-devtools/extension';
 
 import { reducer } from './reducers/reducer';
 
-export const store = createStore(
-  reducer,
-  [],
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+export const store = createStore(reducer, devToolsEnhancer());
